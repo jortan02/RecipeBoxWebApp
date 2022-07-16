c4e59c2b6ea9ffba37929e094a1c47a4b330f224
jortan02 <jortan2002@gmail.com>
2022-07-16 16:55:40 -0600

    Added Recipe model

---
+++ RecipeBox/Models/Recipe.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeBox.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+        public double Hours { get; set; }
+        public int Servings { get; set; }
+        public List<string> Ingredients { get; set; }
+        public List<string> Directions { get; set; }
+
+        public Recipe()
+        {
+
+        }
+    }
+}
